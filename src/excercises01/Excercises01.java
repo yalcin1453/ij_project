20e7594ad5ad6243cd286f0123592f2f110e9a7c
Fatih <yalcinfatihtr@gmail.com>
2020-08-09 17:48:54 -0400

    first job

---
+++ src/excercises01/Excercises01.java
@@ -0,0 +1,17 @@
+package excercises01;
+
+import java.util.HashMap;
+import java.util.Map;
+
+public class Excercises01 {
+
+    public static void main(String[] args) {
+        Map<String,String> list= new HashMap<>();
+        list.put("a","fatih");
+        list.put("b","yalcin");
+        System.out.println(list);
+      
+
+
+
+}}
